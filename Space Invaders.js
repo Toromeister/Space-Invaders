7b8b1053250901e0fb1dff5865c4410134d0494f
Toromeister <48439068+Toromeister@users.noreply.github.com>
2019-03-14 11:15:04 +0100

    Update Space Invaders.js

--- Space Invaders.js
+++ Space Invaders.js
@@ -1,9 +1,49 @@
-var xCanvasSize = 600;
-var yCanvasSize = 400;
+var xCanvasSize = 800;
+var yCanvasSize = 600;
+var sizeEnemy = 35;
+var speedX = 5;
+var speedY = 5;
+var posX = 1;
+var posY = 1;
+var marginY =75;
+var marginX = 100;
 
 function setup(){
     createCanvas(xCanvasSize, yCanvasSize);
-    frameRate(60);
+    rectMode(CENTER);
+    enemyArray = [];
+    for(i=0; i<7; i++){
+        enemyArray[i]=[];
+        for(j=0; j<5; j++){
+            enemyArray[i][j] = new Enemy(90*i+marginX+posX,50*j+25+marginY+posY,sizeEnemy);
+        }
+    }
+}
+
+function draw(){
+    background(51);
+
+    for(i=0; i<enemyArray.length; i++){
+        for(j=0; j<enemyArray[i].length; j++){
+            enemyArray[i][j].show();
+        }
+    }
+
+    posX += speedX;
+
+    for(i=0; i<enemyArray.length; i++){
+        for(j=0; j<enemyArray.length; j++){
+            enemyArray[i][j] = new Enemy(90*i+marginX+posX,50*j+marginY+posY,sizeEnemy);
+
+            if(enemyArray[i][j].y > 500){
+                noLoop();
+            }
+            if(enemyArray[i][j].x > 770 ||enemyArray[i][j].x < 0){
+                posY += speedY;
+                speedX = -speedX;
+            }
+        }
+    }
 }
 
 function keyPressed(){
@@ -21,10 +61,6 @@ function keyPressed(){
 
 }
 
-function draw(){
-    background(51);
-}
+function Enemies(){
 
-function playerShip(){
-    
 }
