298c783b3efafb6fa59a60b617cca5b38f2a1a0f
YngLar <31324793+YngLar@users.noreply.github.com>
2019-03-21 10:59:04 +0100

    Add files via upload

--- space-invaders.js
+++ space-invaders.js
@@ -1,5 +1,6 @@
 var xCanvasSize = 800;
 var yCanvasSize = 600;
+var drops = [];
 var sizeEnemy = 35;
 var speedX = 5;
 var speedY = 5;
@@ -8,6 +9,7 @@ var posY = 1;
 var marginY =75;
 var marginX = 100;
 var enemyArray = [];
+
 
 function setup(){
     createCanvas(xCanvasSize, yCanvasSize);
@@ -18,11 +20,28 @@ function setup(){
             enemyArray[i][j] = new Enemy(90*i+marginX+posX,50*j+25+marginY+posY,sizeEnemy);
         }
     }
+    ship = new Ship(30, 20, 600);
 }
 
 function draw(){
     background(51);
+    ship.show();
+    ship.move();
 
+    //Fjerner skudd
+    for(let i = 0; i < drops.length; i++){
+        drops[i].show();
+        drops[i].move();
+        if(drops[i].y <= 0){
+            drops[i].delete();
+        }
+    }
+    for (var i = drops.length-1; i >= 0; i--) {
+        if (drops[i].toDel) {
+          drops.splice(i, 1);
+        }
+    }
+    //Enemy
     for(i=0; i<enemyArray.length; i++){
         for(j=0; j<enemyArray[i].length; j++){
             enemyArray[i][j].show();
@@ -44,4 +63,27 @@ function draw(){
             }
         }
     }
+    //Bevegelse
+    if(keyIsDown(LEFT_ARROW) && keyIsDown(RIGHT_ARROW)){
+        ship.setDir(0);
+    }else if(keyIsDown(LEFT_ARROW)){
+        ship.setDir(-1);
+    }else if(keyIsDown(RIGHT_ARROW)){
+        ship.setDir(1);
+    }else{
+        ship.setDir(0);
+    }
+    
+    if(ship.x<10){
+        ship.setDir(1);
+    } else if(ship.x > (xCanvasSize-10)){
+        ship.setDir(-1);
+    }
+}
+
+function keyPressed(){
+    if (keyCode === UP_ARROW) {
+        var drop = new Drop(ship.x, height);
+        drops.push(drop);
+      }
 }
