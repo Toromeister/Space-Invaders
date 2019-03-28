9c84efd9820cc8c3329d74b6f142c30fa4b574ca
YngLar <31324793+YngLar@users.noreply.github.com>
2019-03-28 10:21:05 +0100

    Add files via upload

--- enemy.js
+++ enemy.js
@@ -3,10 +3,19 @@ class Enemy{
         this.x = x;
         this.y = y;
         this.size = size;
+        this.toDel = false;
     }
 
     show(){
         fill(255);
-        rect(this.x, this.y, this.size, this.size-6);
+        //rect(this.x, this.y, this.size, this.size-6);
+        ellipse(this.x, this.y, this.size, this.size)
+    }
+    move(xpos, ypos){
+        this.x = xpos;
+        this.y = ypos;
+    }
+    delete(){
+        this.toDel = true;
     }
 }
